07a9ffae5913d94671be77afd14ffd55680f795a
X1XX <dragonhearth1946@gmail.com>
2020-10-10 23:55:24 -0500

    Initial commit Never want to look at this again!

--- src/me/F64/papiexpansions/playtime/ExpansionPlayTime.java
+++ src/me/F64/papiexpansions/playtime/ExpansionPlayTime.java
@@ -8,42 +8,42 @@ import me.F64.PlayTime.Utils.TimeFormat;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
 public class ExpansionPlayTime extends PlaceholderExpansion {
-	public boolean canRegister() {
-		return Bukkit.getPluginManager().getPlugin(getPlugin()) != null;
-	}
+    public boolean canRegister() {
+        return Bukkit.getPluginManager().getPlugin(getPlugin()) != null;
+    }
 
-	public String getAuthor() {
-		return "F64_Rx";
-	}
+    public String getAuthor() {
+        return "F64_Rx";
+    }
 
-	public String getIdentifier() {
-		return "playtime";
-	}
+    public String getIdentifier() {
+        return "playtime";
+    }
 
-	public String getPlugin() {
-		return "PlayTime";
-	}
+    public String getPlugin() {
+        return "PlayTime";
+    }
 
-	public String getVersion() {
-		return "1.0";
-	}
+    public String getVersion() {
+        return "1.0";
+    }
 
-	public String onPlaceholderRequest(Player p, String identifier) {
-		if (p == null) {
-			return "";
-		}
-		if (identifier.equals("player")) {
-			return String.valueOf(p.getName());
-		}
-		if (identifier.equals("time")) {
-			return String.valueOf(TimeFormat.getTime(p.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20));
-		}
-		if (identifier.equals("timesjoined")) {
-			return String.valueOf(p.getStatistic(Statistic.LEAVE_GAME) + 1);
-		}
-		if (identifier.equals("serveruptime")) {
-			return String.valueOf(TimeFormat.Uptime());
-		}
-		return null;
-	}
+    public String onPlaceholderRequest(Player p, String identifier) {
+        if (p == null) {
+            return "";
+        }
+        if (identifier.equals("player")) {
+            return String.valueOf(p.getName());
+        }
+        if (identifier.equals("time")) {
+            return String.valueOf(TimeFormat.getTime(p.getStatistic(Statistic.PLAY_ONE_TICK) / 20));
+        }
+        if (identifier.equals("timesjoined")) {
+            return String.valueOf(p.getStatistic(Statistic.LEAVE_GAME) + 1);
+        }
+        if (identifier.equals("serveruptime")) {
+            return String.valueOf(TimeFormat.Uptime());
+        }
+        return null;
+    }
 }
